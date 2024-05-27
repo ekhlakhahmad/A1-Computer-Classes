06a72930a5cbd19f2fa7401c0b74d62a33892399
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-05-27 10:18:09 +0530

    changed in responsive

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -13,56 +13,56 @@ const Course = () => {
 						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4JBgYVdK_VyHXLzSGNQ5imxh6_6DFRdoPnsziBN01GbpYY517plBU-CmOTWuZ6ZNbqWU&usqp=CAU"
 						alt="course"
 					/>
-					<p>Computer Courses</p>
+					<p className="coursePara">Computer Courses</p>
 				</div>
 				<div className="Box">
 					<img
 						src="https://blogassets.leverageedu.com/blog/wp-content/uploads/2020/05/05011631/How-to-Learn-Spoken-English_.png"
 						alt="course"
 					/>
-					<p>Speaking Course</p>
+					<p className="coursePara">Speaking Course</p>
 				</div>
 				<div className="Box">
 					<img
 						src="https://www.tlctranslation.com/wp-content/uploads/2023/02/DTP.png"
 						alt="course"
 					/>
-					<p>DTP Courses</p>
+					<p className="coursePara">DTP Courses</p>
 				</div>
 				<div className="Box">
 					<img
 						src="https://atijsr.com/img/course/computer-dca.jpeg"
 						alt="course"
 					/>
-					<p>PGDCA Course</p>
+					<p className="coursePara">PGDCA Course</p>
 				</div>
 				<div className="Box">
 					<img
 						src="https://3.imimg.com/data3/XX/RB/MY-11912503/typing-practice.gif"
 						alt="course"
 					/>
-					<p>Computer Typing</p>
+					<p className="coursePara">Computer Typing</p>
 				</div>
 				<div className="Box">
 					<img
 						src="https://online.stanford.edu/sites/default/files/styles/embedded_large/public/2018-03/cs_programminglanguage_cs242.jpg?itok=OMscvbtw"
 						alt="course"
 					/>
-					<p>Programming Languages</p>
+					<p className="coursePara">Programming Languages</p>
 				</div>
 				<div className="Box">
 					<img
 						src="https://zoets.b-cdn.net/wp-content/uploads/2017/04/Public-Speaking-and-Presentation-Skills-Training-Course.png"
 						alt="course"
 					/>
-					<p>Public Speaking</p>
+					<p className="coursePara">Public Speaking</p>
 				</div>
 				<div className="Box">
 					<img
 						src="https://res.cloudinary.com/unichrone/image/upload/v1665149440/Blog/Project-Management-Roles.webp"
 						alt="course"
 					/>
-					<p>Management</p>
+					<p className="coursePara">Management</p>
 				</div>
 			</div>
 		</div>
--- src/components/Facility/Facility.jsx
+++ src/components/Facility/Facility.jsx
@@ -14,28 +14,28 @@ const Facility = () => {
 							src="https://cdn.sanity.io/images/68lp9qid/production/9e54fd568101c6d33018dd431c98b3c059969a15-652x418.png"
 							alt="course"
 						/>
-						<p>CCTV Camara in Classroom</p>
+						<p className="coursePara">CCTV Camara in Classroom</p>
 					</div>
 					<div>
 						<img
 							src="https://images.unsplash.com/photo-1580582932707-520aed937b7b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2Nob29sJTIwY2xhc3Nyb29tfGVufDB8fDB8fHww"
 							alt="course"
 						/>
-						<p>Theory Classroom</p>
+						<p className="coursePara">Theory Classroom</p>
 					</div>
 					<div>
 						<img
 							src="https://5.imimg.com/data5/DJ/PV/GLADMIN-64970456/computer-lab-class.png"
 							alt="course"
 						/>
-						<p>Practical Classroom</p>
+						<p className="coursePara">Practical Classroom</p>
 					</div>
 					<div>
 						<img
 							src="https://cliniconex.com/wp-content/uploads/shutterstock_371601100_1200px-1.jpg"
 							alt="course"
 						/>
-						<p>Waiting Room</p>
+						<p className="coursePara">Waiting Room</p>
 					</div>
 				</div>
 			</div>
