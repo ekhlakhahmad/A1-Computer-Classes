ca8438c3c5ae548acb141efbe2f715807f3c2eff
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-05-29 13:55:22 +0530

    added gallery pages

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,74 @@
 import React from "react";
 import "./style.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoMdCall } from "react-icons/io";
+import Location from "../../components/Location/Location";
 
 const Contact = () => {
-    return <div>
-        
-    </div>;
+	return (
+		<>
+			<div className="contact-sec">
+				<div className="formContainer">
+					<form action="">
+						<div>
+							<label htmlFor="stname">Name:</label>
+							<input type="text" placeholder="Enter Your Name" id="stname" />
+						</div>
+						<div>
+							<label htmlFor="email">E-Mail Id:</label>
+							<input
+								type="email"
+								placeholder="Enter Your Email"
+								inputMode="email"
+								id="email"
+							/>
+						</div>
+						<div>
+							<label htmlFor="number">Mobile No.:</label>
+							<input
+								type="number"
+								placeholder="Enter Your Mobile Number"
+								inputMode="numeric"
+								id="number"
+							/>
+						</div>
+						<div>
+							<textarea
+								name="msg"
+								id="msg"
+								placeholder="Enter Your Message..."></textarea>
+						</div>
+						<div>
+							<input type="submit" value="Submit" />
+						</div>
+					</form>
+				</div>
+				<div className="contactRegisterOffice">
+					<div>
+						<h2 className="underline font-semibold text-2xl mb-4">
+							Register Address
+						</h2>
+						<h3>QUANTUM COMPUTER ACADEMY & MANAGEMENT PVT.LTD.</h3>
+						<h3>
+							Lala Babu Colony, Behind High School Chainpur, Siwan Bihar
+							(841203)
+						</h3>
+						<a
+							className="flex items-center gap-1"
+							href="mailto:quantumcomputeracademy@gmail.com">
+							<MdOutlineEmail />
+							quantumcomputeracademy@gmail.com
+						</a>
+						<a className="flex items-center gap-1" href="tel:+918603156431">
+							<IoMdCall />
+							+91 8603156431
+						</a>
+					</div>
+				</div>
+			</div>
+			<Location />
+		</>
+	);
 };
 
 export default Contact;
